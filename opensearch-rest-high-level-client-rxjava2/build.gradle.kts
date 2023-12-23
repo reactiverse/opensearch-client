81ce607c63ea3085685aa8e054d6dad9fe58dfe1
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-12-23 02:15:23 +0100

    add signing to  modules

--- opensearch-rest-high-level-client-rxjava2/build.gradle.kts
+++ opensearch-rest-high-level-client-rxjava2/build.gradle.kts
@@ -133,5 +133,8 @@ publications {
 }
 
 signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKey, signingPassword)
   sign(publishing.publications["mavenJava"])
 }
